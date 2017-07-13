0bbcdcd3a51052006cbbc6365037453d9b8158e8
Piotr Tecza <piotr.tecza@gmail.com>
2017-07-13 14:31:22 +0100

    property rename

--- Hacathon/Models/Product.cs
+++ Hacathon/Models/Product.cs
@@ -3,7 +3,7 @@
     public class Product
     {
         public int ProductId { get; set; }
-        public string IMAGEURL1 { get; set; }
+        public string ImageUrl1 { get; set; }
         public string ShippingCountryCode { get; set; }
         public string InCountryRanking { get; set; }
         public string ProductGender { get; set; }
